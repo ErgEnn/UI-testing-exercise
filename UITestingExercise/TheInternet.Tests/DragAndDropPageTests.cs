ba66eaafb2327e3971357918ed16c7537f71aa95
Ergo Enn <ergo97@gmail.com>
2019-11-10 21:23:19 +0200

    Change Drag and Drop page tests

---
+++ UITestingExercise/TheInternet.Tests/DragAndDropPageTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TheInternet.Tests
+{
+    [TestClass]
+    public class DragAndDropPageTests : BaseWebPageTest
+    {
+
+        [TestMethod]
+        public void ShouldBeInABOrderByDefault()
+        {
+            new HomePage(Driver).ClickDragAndDropLink();
+            var dnd = new DragAndDropPage(Driver);
+            dnd.Elements().Should().ContainInOrder("A", "B");
+        }
+
+        [TestMethod]
+        public void ShouldBeInBAOrderAfterDragAndDrop()
+        {
+            new HomePage(Driver).ClickDragAndDropLink();
+            var dnd = new DragAndDropPage(Driver);
+            dnd.DragFromColAToColB();
+            dnd.Elements().Should().ContainInOrder("B", "A");
+        }
+
+    }
+}
--- UITestingExercise/TheInternet/DragAndDropPage.cs
+++ UITestingExercise/TheInternet/DragAndDropPage.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 
 namespace TheInternet
 {
-    class DragAndDropPage
+    public class DragAndDropPage : BaseWebDriverUser
     {
+
+        private readonly By _column = By.ClassName("column");
+        private readonly By _columnA = By.Id("column-a");
+        private readonly By _columnB = By.Id("column-b");
+        private readonly By _columnAXPath = By.XPath("/html/body/div[2]/div/div/div/div[1]");
+        private readonly By _columnBXPath = By.XPath("/html/body/div[2]/div/div/div/div[2]");
+
+        public DragAndDropPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public IReadOnlyCollection<IWebElement> Columns() => Driver.FindElements(_column);
+
+        public List<string> Elements() => Columns().Select(element => element.Text).ToList();
+
+        public IWebElement ColumnA() => Driver.FindElement(_columnA);
+
+        public IWebElement ColumnB() => Driver.FindElement(_columnB);
+
+        public void DragFromColAToColB()
+        {
+            Actions builder = new Actions(Driver);
+
+            builder.ClickAndHold(ColumnA()).MoveByOffset(150,0).Release().Build().Perform();
+        }
+
     }
 }
